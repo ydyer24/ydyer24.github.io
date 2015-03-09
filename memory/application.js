a124a0809a1cdfdbb1907611d1e3026e9c172109
ydyer24 <ydyer@kippnyccp.org>
2015-03-09 13:06:11 -0400

    Create application.js

---
+++ memory/application.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  var card_array = ["fish", "fish", "turtle", "turtle", "pig", "pig", "dog", "dog", "cat", "cat", "snake", "snake", "bird", "bird", "goat", "goat", "hamster", "hamster", "dragon", "dragon"]
+
+  for(i in card_array){
+    $('#card_holder').append('<div class="card"><p>'+card_array[i]+'</p></div>');
+  }
+});
