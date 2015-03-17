2e8b7f20592e05c1c8af4cc47813e1688d56ed1e
ydyer24 <ydyer@kippnyccp.org>
2015-03-17 13:34:28 -0400

    Update application.js

--- memory/application.js
+++ memory/application.js
@@ -1,7 +1,26 @@
 $(document).ready(function(){
-  var card_array = ["fish", "fish", "turtle", "turtle", "pig", "pig", "dog", "dog", "cat", "cat", "snake", "snake", "bird", "bird", "goat", "goat", "hamster", "hamster", "dragon", "dragon"]
-
+  var card_array = ["fish", "fish", "turtle", "turtle", "pig", "pig", "dog", "dog", "cat", "cat", "snake", "snake", "bird", "bird", "hamster", "hamster", "goat", "goat", "dragon", "dragon"];
+  var click_count = 0;//above is the name of the cards
+  var click_1 = 'nothing';
+  var click_2 = 'nothing';
   for(i in card_array){
-    $('#card_holder').append('<div class="card"><p>'+card_array[i]+'</p></div>');
-  }
+    $('#card_holder').append('<div class="card"><p>'+card_array[i]+'</p></div>');//this function helps store the different cards
+  };
+  $('.card').click(function(){
+    if(click_count == 0){
+  	$(this).find('p').css('opacity', 1);
+    $(this).find('p').addClass('clicked'); //this fucntion helps make the card visible, and puts it into a class 
+      click_count = 1;
+      click_1 = $(this).find('p').text();
+    }
+    else{	
+      $(this).find('p').css('opacity', 1);
+      click_count = 0;
+      click_2 = $(this).find('p').text();
+      $(this).find('p').addClass('clicked');
+      if(click_1 != click_2){
+         setTimeout(function(){$('.clicked').css('opacity', '0').removeClass('clicked')}, 1000);
+      }
+    };
+	});
 });
