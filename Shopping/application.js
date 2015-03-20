290605ccadda09d91f0aa3819bdf17c1227915ec
ydyer24 <ydyer@kippnyccp.org>
2015-03-20 13:36:36 -0400

    Update application.js

--- Shopping/application.js
+++ Shopping/application.js
@@ -1,1 +1,7 @@
-
+$(document). ready(function(){
+  // var Click_count= 0;
+  //   $('#card_holder').append('<div class=Shopping_list"><imput>
+  function disable() { 
+     document.getElementById("mySelect").disable =true
+  }
+})
