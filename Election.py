053a2f512ec3ff2d08b1f3669c0f55d52f289d09
ydyer24 <ydyer@kippnyccp.org>
2015-05-13 13:23:44 -0400

    Update Election.py

--- Election.py
+++ Election.py
@@ -1,1 +1,9 @@
-
+from random import random
+for i in range(10000):
+  regions = 0
+  if radint (0,100) <= 87:
+    regions = regions +1
+    if radint (0,100) <=65:
+      regions = regions +1
+      if radint (0,100) <=17:
+        regions = regions +1
