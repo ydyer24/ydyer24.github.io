09ba025c0b4a680e2d90f4b9f8745c0a5ddbcadb
ydyer24 <ydyer@kippnyccp.org>
2015-05-13 13:36:03 -0400

    Update Election.py

--- Election.py
+++ Election.py
@@ -7,3 +7,15 @@ for i in range(10000):
       regions = regions +1
       if radint (0,100) <=17:
         regions = regions +1
+          if random() <=region1:
+    region_wins=region_wins+1
+#basically here you are checking if the region is equal or more to wins then add 1 to get the total of that regent.
+  if random()<=region2:
+    region_wins=region_wins+1
+  if random()<=region3: 
+    region_wins=region_wins+1
+  if region_wins >= 2;
+    total_wins +=1
+    
+percentage=total_wins/float(10000)#the objective is to find the total percentage of wins in this section.
+print ("The chances of Winning are (percentage)")
