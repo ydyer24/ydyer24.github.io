fea00cc9b96a6ab4cdcd70be7058936b1edbf5b1
ydyer24 <ydyer@kippnyccp.org>
2015-05-13 13:49:41 -0400

    Update Election.py

--- Election.py
+++ Election.py
@@ -3,9 +3,9 @@ for i in range(10000):
   regions = 0
   if radint (0,100) <= 87:
     regions = regions +1
-    if radint (0,100) <=65:
+  if radint (0,100) <=65:
       regions = regions +1
-      if radint (0,100) <=17:
+  if radint (0,100) <=17:
         regions = regions +1
   percentage=total_wins/float(10000)#the objective is to find the total percentage of wins in this section.
 print ("The chances of Winning are (percentage)")
